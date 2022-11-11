9b8148e7086edfd0f0623121b06f3b00d81c27b2
Alec Leamas <leamas.alec@nowhere.net>
2022-11-11 23:59:26 +0100

    Plugin.cmake: Link to correct ocpn::sqlite

--- Plugin.cmake
+++ Plugin.cmake
@@ -93,7 +93,7 @@ macro(add_plugin_libraries)
   target_link_libraries(${PACKAGE_NAME} ocpn::plugingl)
   
   add_subdirectory("${CMAKE_SOURCE_DIR}/opencpn-libs/sqlite")
-  target_link_libraries(${PACKAGE_NAME} sqlite::sqlite)  
+  target_link_libraries(${PACKAGE_NAME} ocpn::sqlite)  
 
   # The wxsvg library enables SVG overall in the plugin
   add_subdirectory("${CMAKE_SOURCE_DIR}/opencpn-libs/wxsvg")
